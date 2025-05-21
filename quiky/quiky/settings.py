a02c8527d5c879f808681e301a5e6368380dc822
garima-l5 <garima.sachdeva@layerfive.com>
2025-05-22 01:57:34 +0530

    setting

--- quiky/quiky/settings.py
+++ quiky/quiky/settings.py
@@ -129,12 +129,3 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your-email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your-app-password'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
