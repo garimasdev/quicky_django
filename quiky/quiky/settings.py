4914efea7485c19c0861a1329105ae91309ac3b4
garima-l5 <garima.sachdeva@layerfive.com>
2025-05-22 13:50:32 +0530

    changes in settings

--- quiky/quiky/settings.py
+++ quiky/quiky/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ck$okw*9=%+b5ab*sju*hc_et!_gkku)ib%1n%=)o5lb#m1qt9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["quikymeals.com"]
 
 
 # Application definition
@@ -128,13 +128,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your-email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your-app-password'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
